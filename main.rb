589eada195140c3c4de6f013d4ab3f53ca274dd5
Dennis Ojeda <royojeda1@gmail.com>
2022-05-14 06:39:31 +0800

    Add a #build_tree method
    Turns an array to a balanced binary tree of Node objects.

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ include Comparable
 
 class Node
   attr_reader :data
+  attr_accessor :left, :right
 
   def <=>(other)
     data <=> other.data
@@ -15,8 +16,31 @@ class Node
 end
 
 class Tree
-  def initialize(arr)
-    @arr = arr
-    @root = nil
+  attr_reader :input
+  attr_accessor :root
+
+  def initialize(input)
+    @input = input.uniq.sort
+    @root = build_tree(self.input)
+  end
+
+  def build_tree(arr, start = 0, fin = arr.length - 1)
+    return if start > fin
+
+    mid = (start + fin) / 2
+    root = Node.new(arr[mid])
+
+    root.left = build_tree(arr, start, mid - 1)
+    root.right = build_tree(arr, mid + 1, fin)
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
+
+a = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+a.pretty_print
