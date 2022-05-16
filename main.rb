9b1ee625042745e18e4aacf11e616c3e8aeff837
Dennis Ojeda <royojeda1@gmail.com>
2022-05-17 06:35:59 +0800

    Add a #rebalance method
    Rebalances an unbalanced Tree

--- main.rb
+++ main.rb
@@ -183,14 +183,23 @@ class Tree
 
     true
   end
+
+  def rebalance
+    return if balanced?
+
+    self.root = build_tree(preorder)
+  end
 end
 
 a = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 a.insert(999)
 a.insert(24)
-# a.insert(25)
+a.insert(25)
 a.insert(22)
-# a.insert(21)
-# a.insert(20)
+a.insert(21)
+a.insert(20)
+a.pretty_print
+p a.balanced?
+a.rebalance
 a.pretty_print
 p a.balanced?
