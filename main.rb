8cc484e0461ee785c00f1d5964c8d4573ccd7d9e
Dennis Ojeda <royojeda1@gmail.com>
2022-05-16 20:18:58 +0800

    Add a #preorder method
    Traverses the Tree in a depth-first preorder manner, and yields each
    Node to the given block. If no block is passed, an array of the Nodes is
    returned.

--- main.rb
+++ main.rb
@@ -129,9 +129,24 @@ class Tree
 
     return arr unless block_given?
   end
+
+  def preorder(current_node = root, &proc)
+    return if current_node.nil?
+
+    unless block_given?
+      arr = []
+      proc = proc { |node| arr.push(node.data) }
+    end
+
+    preorder(current_node.left, &proc) unless current_node.left.nil?
+    proc.call(current_node)
+    preorder(current_node.right, &proc) unless current_node.right.nil?
+
+    return arr unless block_given?
+  end
 end
 
 a = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 a.pretty_print
-a.inorder { |node| puts "#{node.data} asd" }
-p a.inorder
+a.preorder { |node| puts "#{node.data} asd" }
+p a.preorder
